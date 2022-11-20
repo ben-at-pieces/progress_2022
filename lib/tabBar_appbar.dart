de02d9d8d0f0e520a3120ab3cade9844f56c0b78
Ben <ben@pieces.app>
2022-11-19 23:55:07 -0500

    Added polish to Bar Graph UI Bottom labels with qty
    saved
    updated
    Tagged
    shared
    person
    related links
    
    TODO --> get the pop-over logic into our local Bar Graph

--- lib/tabBar_appbar.dart
+++ lib/tabBar_appbar.dart
@@ -84,11 +84,11 @@ class HomePageState extends State<HomePageAppBar> {
           ), // AppBar
           body: TabBarView(
             children: [
-              /// Circular Pie Chart ==========================================================
-              HomePagePie(),
-
               /// Vertical bar graph ==========================================================
               BarGraph(),
+
+              /// Circular Pie Chart ==========================================================
+              HomePagePie(),
 
               ///TODO work in another widget
               // PieChartSample1(),
